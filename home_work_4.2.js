b3ec18774e19ad3383a0433a2f4b3649b2916c47
Victor-Eighth <fomin.nickname@gmail.com>
2024-09-23 18:32:32 +0300

    Update home_work_4.2.js
    ok
    
    Co-authored-by: anatoly-karpovich <84677425+anatoly-karpovich@users.noreply.github.com>

--- home_work_4.2.js
+++ home_work_4.2.js
@@ -4,11 +4,13 @@ let age = 59
 if (typeof age !== "number"){
     console.log(typeof age)
     console.log("Incorrect data type")
-}
+} else {
 if (age < minAge){
     console.log("You don't have acess cause your age is " + age + " It's less then")
 } else if (age >= minAge && age < maxAge){
     console.log("Welcome!")
 } else if (age > maxAge){
     console.log("Keep calm and look Culture channel")
-} else {console.log("Technical work!")}
+} else {console.log("Technical work!")
+}
+}
