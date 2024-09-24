62e54e63fb31dcd2c6be59c440be88492d1f4322
Victor-Eighth <v.pronicheff@yandex.ru>
2024-09-24 19:40:20 +0300

    homework5_task2

---
+++ home_work_5.2.js
@@ -0,0 +1,12 @@
+let minDigit = 1
+let maxDigit = 100
+let digit = 99
+
+if (digit % 3 === 0 && digit % 5 === 0) {
+    console.log("Число - делится на 3 и на 5")
+} else if (digit % 5 === 0){
+    console.log("Число - делится на 5")
+}  else if (digit % 3 === 0){
+    console.log("Число - делится на 3")
+} 
+
